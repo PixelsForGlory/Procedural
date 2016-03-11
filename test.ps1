c675fceeca19a0155b554f7938a3c206413a7278
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 18:54:08 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-﻿# Run unit test
+# Run unit test
 Copy-Item $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMesh\bin\Release\ProceduralVoxelMesh.* -Destination $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTest\bin\Release\ -Force
 vstest.console $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTest\bin\Release\ProceduralVoxelMeshTest.dll /Settings:$env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTest\test.runsettings /logger:Appveyor
 
@@ -6,4 +6,4 @@ vstest.console $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTest\bin\Release\Pr
 Copy-Item $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMesh\bin\Release\ProceduralVoxelMesh.* -Destination $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTester\Assets\Plugins\ -Force
 Copy-Item $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshEditor\bin\Release\ProceduralVoxelMeshEditor.* -Destination $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTester\Assets\Plugins\Editor\ -Force
 
-& $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTester\CompareImages.ps1
+. $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTester\CompareImages.ps1
