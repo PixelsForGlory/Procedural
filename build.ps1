104e45f813ab8b586a8fdd334210fc73a431a27a
afuzzyllama <afuzzyllama@users.noreply.github.com>
2016-03-11 10:53:53 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-msbuild $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMesh.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:Configuration=Release /p:Platform="Any CPU"
+msbuild $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMesh.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:Configuration=Release /p:Platform="Any CPU" /p:ReferencePath="$env:DEPENDENCIES_DIR\Unity\Editor\Data\Managed"
