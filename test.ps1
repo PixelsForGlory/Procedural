ae024741ac5cff252da7586bc7c3074671b315af
afuzzyllama <karcieri@gmail.com>
2016-06-12 18:41:36 -0400

    Improving build process

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
 # Run unit test
 Copy-Item $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMesh\bin\Release\ProceduralVoxelMesh.* -Destination $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTest\bin\Release\ -Force
 vstest.console $env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTest\bin\Release\ProceduralVoxelMeshTest.dll /Settings:$env:APPVEYOR_BUILD_FOLDER\ProceduralVoxelMeshTest\test.runsettings /logger:Appveyor
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
