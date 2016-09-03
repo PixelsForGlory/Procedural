7fd8c1642c4576946642b05779955e922a3fd568
afuzzyllama <karcieri@gmail.com>
2016-09-03 11:46:28 -0400

    Fixed package script

--- packageBuild.ps1
+++ packageBuild.ps1
@@ -1,10 +1,10 @@
 $destinationFolder = "$env:STAGING_DIR\ReleaseContents\Plugins\"
 if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
-Copy-Item $env:APPVEYOR_BUILD_FOLDER\Procedural\bin\Release\Procedural.dll -Destination $destinationFolder -Force 
+Copy-Item $env:APPVEYOR_BUILD_FOLDER\Procedural\bin\Release\PixelsForGlory.Procedural.dll -Destination $destinationFolder -Force 
 
 $destinationFolder = "$env:STAGING_DIR\ReleaseContents\Plugins\Editor\"
 if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
-Copy-Item $env:APPVEYOR_BUILD_FOLDER\ProceduralEditor\bin\Release\ProceduralEditor.dll -Destination $destinationFolder -Recurse -Force 
+Copy-Item $env:APPVEYOR_BUILD_FOLDER\ProceduralEditor\bin\Release\PixelsForGlory.Procedural.Editor.dll -Destination $destinationFolder -Recurse -Force 
 
 $destinationFolder = "$env:STAGING_DIR\ReleaseContents\"
 if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
